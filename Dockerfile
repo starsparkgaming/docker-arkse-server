97f0cdbbe8daa2eb899a3e05216edccd573ea285
starsparkgaming <79425116+starsparkgaming@users.noreply.github.com>
2021-02-24 15:26:35 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM        ubuntu:latest
+
+RUN         dpkg --add-architecture i386 \
+            && apt update \
+            && apt upgrade -y \
+            && apt install -y tar curl lib32gcc1 libgcc1 ca-certificates \
+            && useradd -m -d /home/container container \
+			&& mkdir /data \
+
+USER        container
+ENV         HOME /home/container
+WORKDIR     /home/container
+
+VOLUME		/data
+
+COPY        ./entrypoint.sh /entrypoint.sh
+CMD         ["/bin/bash", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/container
+## download and install steamcmd
+mkdir /home/container/steamcmd
+curl -SL -o steamcmd.tar.gz "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
+tar -xzvf steamcmd.tar.gz -C /home/container/steamcmd
+rm /home/container/steamcmd.tar.gz
+
+# install steamm app with steamcmd
+./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container/app +app_update 376030 +quit
+
+# link important folders
+ln -s /home/container/app/ShooterGame/Saved /data/saved
+ln -s /home/container/app/ShooterGame/Saved/Config /data/config
+ln -s /home/container/app/ShooterGame/Content/Mods /data/mods
+
+# run the Server
+eval /home/container/app/ShooterGame/Binaries/Linux/ShooterGameServer
